cf1a0565b82f8446e4c276ed11629e9736bc72ac
tompet0191 <skylines123@gmail.com>
2018-05-02 09:43:42 +0200

    Cleanup

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace KeyPressed
 {
-    static class Program
+    static class KeyPressed
     {
         /// <summary>
         /// The main entry point for the application.
